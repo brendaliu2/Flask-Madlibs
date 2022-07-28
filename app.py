75d020955e206e8754744a0ad9ac20ca790c1f6a
Brenda Liu <brenda.liu24@gmail.com>
2022-07-27 19:04:21 -0500

    done with madlibs exercise for results

--- app.py
+++ app.py
@@ -8,9 +8,24 @@ app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
 
+
+
+
 @app.get('/')
 def questions():
-
+    """takes story instance prompts and creates form to display"""
     questions = silly_story.prompts
     return render_template('questions.html',
                             questions = questions)
+
+
+@app.get('/results')
+def results():
+    '''takes inputs from form and generate story to display'''
+    # answers = {}
+    # questions = silly_story.prompts
+    # for question in questions:
+    #     answers[question] = request.args[question] #can just pass in request.args
+    text = silly_story.generate(request.args)
+    return render_template('results.html',
+                            text = text)
