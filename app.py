b5128265bc17df734bd94324dcd59b17cc033143
Brenda Liu <brenda.liu24@gmail.com>
2022-07-27 19:29:13 -0500

    tried further study

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 from flask_debugtoolbar import DebugToolbarExtension
 
-from stories import silly_story
+from stories import silly_story, excited_story
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
@@ -9,12 +9,17 @@ app.config['SECRET_KEY'] = "secret"
 debug = DebugToolbarExtension(app)
 
 
-
-
 @app.get('/')
+def load_home_page():
+    '''displays drop down form to choose story template'''
+
+    return render_template('homepage.html')
+
+@app.get('/questions')
 def questions():
     """takes story instance prompts and creates form to display"""
-    questions = silly_story.prompts
+    story_type = request.args["story-type"]
+    questions = globals()[story_type].prompts
     return render_template('questions.html',
                             questions = questions)
 
